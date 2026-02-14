a8a8e8f6bc23a1354d9e69c8e684e4c8912a6369
Gabriel Santos <gabriel.osantos@sptech.school>
2026-02-13 23:33:39 -0300

    Refactor RoutesApp component and clean imports

--- careplus/src/routes.jsx
+++ careplus/src/routes.jsx
@@ -1,24 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
-import SideBar from './components/sidebar/SideBar'
-import  TelaLogin  from './pages/login/TelaLogin'
-import Funcionarios from './pages/funcionarios/Funcionarios'
-import TelaDashboard from './pages/telaDashboard/TelaDashboard'
-import ConsultaAtual from './pages/consultaAtual/ConsultaAtual'
+import SideBar from "./components/sidebar/SideBar"
+import TelaLogin from "./pages/login/TelaLogin"
+import Funcionarios from "./pages/funcionarios/Funcionarios"
+import TelaDashboard from "./pages/telaDashboard/TelaDashboard"
 import FichaClinica from "./pages/fichaClinica/FichaClinica"
 
-
-export default function RoutesApp(){
-    return(
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<TelaLogin/>} />
-                <Route path="/funcionarios" element={<Funcionarios/>} />
-                <Route path='/dashboard' element={<TelaDashboard/>} />
-                <Route path='/consulta-atual' element={<ConsultaAtual/>} />
-                <Route path="/ficha-clinica" element={<FichaClinica />} />
-            </Routes>
-        </BrowserRouter>
-    )
+export default function RoutesApp() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<TelaLogin />} />
+        <Route path="/funcionarios" element={<Funcionarios />} />
+        <Route path="/dashboard" element={<TelaDashboard />} />
+        <Route path="/ficha-clinica" element={<FichaClinica />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
-
