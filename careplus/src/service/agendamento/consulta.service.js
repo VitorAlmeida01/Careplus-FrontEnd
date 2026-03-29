845790f6aa206ad74bca3541730b8f17e9e27cf3
Grigor-MF <grigor.ferreira@sptech.school>
2026-03-29 19:14:32 -0300

    feat: Modal de cadastro de consulta integrado

--- careplus/src/service/agendamento/consulta.service.js
+++ careplus/src/service/agendamento/consulta.service.js
@@ -14,3 +14,39 @@ export async function buscarPacientePorNome(name){
         return null;
     }
 }
+
+export async function marcarConsulta(pacienteId, funcionarioId, dataHora, tipo){
+   if(!pacienteId || !funcionarioId || !dataHora || !tipo){
+       console.warn("Marcação de consulta cancelada: um ou mais parâmetros estão indefinidos.");
+       return null;
+   }
+
+   try {
+       const response = await api.post("/consultas-prontuario", {
+           pacienteId,
+           funcionarioId,
+           dataHora,
+           tipo
+       });
+       return response.data;
+   } catch (error) {
+       console.error("Erro na requisição:", error);
+       return null;
+   }
+
+}
+
+export async function marcarConsultaRecorrente(payload){
+    if(!payload){
+        console.warn("Marcação de consulta recorrente cancelada: payload está indefinido.");
+        return null;
+    }
+
+    try {
+        const response = await api.post("/consultas-prontuario/recorrentes", payload);
+        return response.data;
+    } catch (error) {
+        console.error("Erro na requisição:", error);
+        return null;
+    }
+}
